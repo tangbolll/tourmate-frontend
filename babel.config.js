48eaaf3f6fc639b7b1775d373a7a622dadf32d67
higongseo <hwi02@hanyang.ac.kr>
2025-03-28 23:38:56 +0900

    initial page routing with expo-router

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,11 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: [
-            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-            "nativewind/babel",
-        ]
-        ,plugins: ['nativewind/babel'],
+            "babel-preset-expo", // Expo의 기본 preset
+            "nativewind/babel",  // NativeWind 설정을 presets에 추가
+        ],
+        plugins: [
+            // 여기에 필요한 다른 플러그인들을 추가할 수 있습니다
+        ],
     };
 };
