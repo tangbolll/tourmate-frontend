76aa5e98101a738495a7bc31b07929be2a233e28
tangbolll <tangboll5075@gmail.com>
2025-03-30 20:27:47 +0900

    여행기간/모집기간..

--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import EventHeader from '../components/EventHeader';
+import EventSchedule from '../components/EventSchedule';
+
 
 export default function App() {
     return (
+        
         <SafeAreaView className="flex-1">
         <StatusBar style="auto" />
         <EventHeader 
@@ -11,7 +14,16 @@ export default function App() {
             participantsCurrent={3} 
             participantsTotal={5} 
         />
+        <EventSchedule 
+            eventDate="3월4일(월)"
+            eventTimeStart="15:00"
+            eventTimeEnd="19:00"
+            recruitStart="3월1일(금)"
+            recruitEnd="3월3일(일)"
+        />
         {}
         </SafeAreaView>
+
+        
     );
 }
