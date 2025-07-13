edfd62cb0121351d51de2b1f4a62768852dd9dda
higongseo <hwi02@hanyang.ac.kr>
2025-07-13 21:30:45 +0900

    [design] 홈 탭 UI수정 1차 완료

--- components/home/Post.js
+++ components/home/Post.js
@@ -239,7 +239,7 @@ const styles = StyleSheet.create({
     actionButton: {
         flexDirection: 'row',
         alignItems: 'center',
-        marginLeft: 16,
+        marginLeft: 8,
     },
     actionText: {
         marginLeft: 4,
