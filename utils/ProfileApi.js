58afca0718185a743f427b485624a4b554ea61a5
tangbolll <tangboll5075@gmail.com>
2025-08-25 15:17:42 +0900

    [design] 회원가입/환경설정

--- utils/ProfileApi.js
+++ utils/ProfileApi.js
@@ -1,7 +1,66 @@
+import Constants from 'expo-constants';
+import { Platform, Alert } from 'react-native';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://localhost:8080';
+// API 베이스 URL 설정
+const getBaseURL = () => {
+    if (__DEV__) {
+        if (Platform.OS === 'android') {
+            return 'http://10.0.2.2:8080';
+        }
+        return Constants.expoConfig?.extra?.API_BASE_URL_DEV;
+    } else {
+        return Constants.expoConfig?.extra?.API_BASE_URL_PROD;
+    }
+};
+const API_URL = getBaseURL();
+
+
+// ------------------  Login  ------------------
+
+// 로그인 함수
+export const handleLoginApi = async (email, password, signIn) => {
+  if (!email || !password) {
+    Alert.alert('오류', '이메일과 비밀번호를 모두 입력해주세요.');
+    return;
+  }
+
+  try {
+    const response = await axios.post(`${API_URL}/api/auth/login`, {
+      email,
+      password,
+    });
+
+    if (response.status === 200 && response.data.token) {
+      const { token, userId } = response.data;
+      await signIn(token, userId);
+      Alert.alert('성공', '로그인되었습니다.');
+    } else {
+      Alert.alert('로그인 실패', '서버에서 오류가 발생했습니다.');
+    }
+  } catch (error) {
+    console.error("Login Failed:", error.response?.data || error.message);
+    Alert.alert('로그인 실패', `이메일 또는 비밀번호가 일치하지 않습니다.`);
+  }
+};
+
+// ------------------  Register  ------------------
+
+export const registerUserApi = async (userData) => {
+  try {
+    const response = await axios.post(`${API_URL}/api/auth/register`, userData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data; // 성공 시, 응답 데이터를 반환
+  } catch (error) {
+    throw error.response?.data || error.message; // 실패 시, 에러를 throw
+  }
+};
 
 export const fetchUserProfileApi = async (userId) => {
     try {
@@ -45,4 +104,15 @@ export const checkNicknameApi = async (nickname) => {
         console.error('Error checking nickname:', error);
         throw error;
     }
+};
+
+export const checkEmailApi = async (email) => {
+  try {
+    const response = await axios.get(`${API_URL}/api/auth/check-email?email=${email}`);
+    return response.data; // 서버 응답 (true/false)을 직접 반환
+  } catch (error) {
+    console.error("Email check failed:", error.response?.data || error.message);
+    // 에러를 UI 컴포넌트에서 처리할 수 있도록 throw
+    throw new Error('이메일 확인 중 오류가 발생했습니다.');
+  }
 };
